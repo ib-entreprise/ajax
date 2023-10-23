d3c82c38c4ee3aa3d50b58c4abe877c55402d368
ib-entreprise <bahibrahimasory590@email.com>
2023-10-23 16:38:45 +0200

    exo 2 ok we can select dog by race

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let button = document.querySelector('#boutonChien');
-button.addEventListener('click', findDog);
+let raceChien = document.querySelector('#raceChien');
+raceChien.addEventListener('change', findDog);
+raceChien.addEventListener('click', findDog);
 
-function findDog(){
-    // ici on tente d'acceder a l'url qui remplace notre base de donnee
-    fetch('https://dog.ceo/api/breeds/image/random')
-        .then(response => response.json())  // ici on recupere la reponse
+
+function findDog(){ 
+    // ici on filtre au niveau de l'url pour specifier la race 
+    const selectedRace = document.querySelector('#raceChien').value;
+    
+    fetch(`https://dog.ceo/api/breed/${selectedRace}/images/random`)
+    .then(response => response.json())  // ici on recupere la reponse
         .then(data => {       // et la on fait de ce qu'on veut de la reponse
             if (data.status === 'success') {
+                console.log(data);
                 const imageUrl = data.message;                
-                displayDogImage(imageUrl);
+                displayDogImage(imageUrl,selectedRace);
             } else {
                 alert('Une erreur s\'est produite le chien n\'a pu etre recupere.');
             }
@@ -19,10 +24,13 @@ function findDog(){
 }
 
 // on charge l'image recuperer dans le fetch sur notre page web
-function displayDogImage(imageUrl) { 
+function displayDogImage(imageUrl,selectedRace) { 
     const dogImage = document.querySelector('#content img');
     dogImage.src = imageUrl;
     const divContent = document.querySelector('#content');
+    const imageName = document.querySelector('h2');
+    imageName.textContent = selectedRace;
+
     divContent.style.background="#fff";
     
 }
