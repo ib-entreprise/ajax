6720cd1355a6a91f9f126c42db377b61828bb55e
ib-entreprise <bahibrahimasory590@email.com>
2023-10-24 00:36:35 +0200

    exo 3 ok

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 let raceChien = document.querySelector('#raceChien');
-raceChien.addEventListener('change', findDog);
-raceChien.addEventListener('click', findDog);
+let afficheChien = document.querySelector('button');
+let search = document.querySelector('#search');
+let image = document.querySelector('#image');
 
+raceChien.addEventListener('change', findDog);
+image.addEventListener('click', favorites);
+afficheChien.addEventListener('click', findDogRandom);
+
+
+function favorites(){    
+    let liste = document.querySelector('#favorites');
+    let itemFavorites = document.createElement('li');
+    let itemFavoritesImg = document.createElement('img'); 
+    itemFavoritesImg.src = image.src;
+
+    itemFavorites.appendChild(itemFavoritesImg);
+    liste.appendChild(itemFavorites);
+
+
+}
 
 function findDog(){ 
-    // ici on filtre au niveau de l'url pour specifier la race 
-    const selectedRace = document.querySelector('#raceChien').value;
-    
-    fetch(`https://dog.ceo/api/breed/${selectedRace}/images/random`)
+     // ici on filtre au niveau de l'url pour specifier la race 
+     const selectedRace = document.querySelector('#raceChien').value;
+     fetch(`https://dog.ceo/api/breed/${selectedRace}/images/random`)
+     .then(response => response.json())  // ici on recupere la reponse
+         .then(data => {       // et la on fait de ce qu'on veut de la reponse
+             if (data.status === 'success') {                 
+                 const imageUrl = data.message;                
+                 displayDogImage(imageUrl,selectedRace);
+             } else {
+                 alert('Une erreur s\'est produite le chien n\'a pu etre recupere.');
+             }
+         })
+         .catch(error => {
+             console.error('Erreur de fetch : ', error);
+         });
+   
+}
+function findDogRandom(){ 
+    // ici on recupere un images l'url sans specifier la race  
+    fetch(`https://dog.ceo/api/breeds/image/random`)
     .then(response => response.json())  // ici on recupere la reponse
         .then(data => {       // et la on fait de ce qu'on veut de la reponse
             if (data.status === 'success') {
                 console.log(data);
                 const imageUrl = data.message;                
-                displayDogImage(imageUrl,selectedRace);
+                displayDogImage(imageUrl,imageUrl);
             } else {
                 alert('Une erreur s\'est produite le chien n\'a pu etre recupere.');
             }
@@ -30,7 +63,6 @@ function displayDogImage(imageUrl,selectedRace) {
     const divContent = document.querySelector('#content');
     const imageName = document.querySelector('h2');
     imageName.textContent = selectedRace;
-
     divContent.style.background="#fff";
     
 }
